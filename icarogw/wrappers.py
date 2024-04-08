f1dbc8d5c80994597eee941b426e52b1dc18bfef
simone.mastrogiovanni <simone.mastrogiovanni@ligo.org>
2024-04-08 05:54:31 -0700

    rate function for total mass

--- icarogw/wrappers.py
+++ icarogw/wrappers.py
@@ -208,18 +208,20 @@ class mass_ratio_prior_Gaussian(pm_prob):
         p1=TruncatedGaussian(kwargs['mu_q'],kwargs['sigma_q'],0.,1.)
         self.prior=p1
 
-class lowSmoothedwrapper(object):
+class mass_ratio_prior_Powerlaw(pm_prob):
+    def __init__(self):
+        self.population_parameters=['alpha_q']
+    def update(self,**kwargs):
+        self.prior=PowerLaw(0.,1.,kwargs['alpha_q'])
+
+class lowSmoothedwrapper(pm_prob):
    def __init__(self, mw):
         self.population_parameters = ['delta_m'] + mw.population_parameters
         self.mw = mw
    def update(self,**kwargs):
         self.mw.update(**{key:kwargs[key] for key in self.mw.population_parameters})
-        self.p1 = LowpassSmoothedProb(self.mw.prior,kwargs['delta_m'])
-   def pdf(self,m):
-        return self.p1.pdf(m)
-   def log_pdf(self,m):
-        return self.p1.log_pdf(m)
-
+        self.prior = LowpassSmoothedProb(self.mw.prior,kwargs['delta_m'])
+ 
 # A parent class for the standard mass probabilities
 # LVK Reviewed
 class pm1m2_prob(object):
