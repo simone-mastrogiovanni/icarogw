3f3e08f1b5d161af37eb9cce8f9416862efbf9a3
Sarah Ferraiuolo <sarah.ferraiuolo@univ-amu.fr>
2025-03-26 16:30:31 +0100

    added code comments

--- icarogw/likelihood.py
+++ icarogw/likelihood.py
@@ -10,7 +10,7 @@ from .wrappers import FlatLambdaCDM_wrap
 class hierarchical_likelihood(bilby.Likelihood):
     def __init__(self, posterior_samples_dict, injections, rate_model, nparallel=None, neffPE=20,neffINJ=None):
         '''
-        Base class for an hierachical liklihood. It just saves all the input requirements for a general hierarchical analysis
+        Base class for an hierachical likelihood. It just saves all the input requirements for a general hierarchical analysis
         
         Parameters
         ----------
@@ -61,17 +61,18 @@ class hierarchical_likelihood(bilby.Likelihood):
         # are not enough to calculate the selection effect
         
         xp = get_module_array(self.injections.log_weights)
-        
+
         if (Neff<self.neffINJ) | (Neff==0.):
             return float(xp.nan_to_num(-xp.inf))
-        
+
         # Update the weights on the PE
         self.posterior_samples_dict.update_weights(self.rate_model)
         if xp.any(self.posterior_samples_dict.get_effective_number_of_PE()<self.neffPE):
             return float(xp.nan_to_num(-xp.inf))
         
         integ=self.posterior_samples_dict.log_weights # Extract a matrix N_ev X N_samples of log weights
-             
+        #print('log 1',self.posterior_samples_dict.n_ev*xp.log(self.injections.Tobs))
+        #print('log 3',xp.sum(xp.log(self.posterior_samples_dict.sum_weights)))  
         # Combine all the terms  
         if self.rate_model.scale_free:
             # Log likelihood for scale free model, Eq. 1.3 on the document
@@ -80,7 +81,8 @@ class hierarchical_likelihood(bilby.Likelihood):
             Nexp=self.injections.expected_number_detections()
             # Log likelihood for  the model, Eq. 1.1 on the document
             log_likeli = -Nexp + self.posterior_samples_dict.n_ev*xp.log(self.injections.Tobs)+xp.sum(xp.log(self.posterior_samples_dict.sum_weights))
-        
+        #print('Nexp',-Nexp)  
+
         # Controls on the value of the log-likelihood. If the log-likelihood is -inf, then set it to the smallest
         # python valye 1e-309
         if log_likeli == xp.inf:
@@ -97,7 +99,7 @@ class hierarchical_likelihood(bilby.Likelihood):
 class hierarchical_likelihood_noevents(bilby.Likelihood):
     def __init__(self, injections, rate_model):
         '''
-        Base class for an hierachical liklihood. It just saves all the input requirements for a general hierarchical analysis
+        Base class for an hierachical likelihood. It just saves all the input requirements for a general hierarchical analysis
         
         Parameters
         ----------
@@ -143,26 +145,57 @@ class hierarchical_likelihood_noevents(bilby.Likelihood):
 
 
 class Poisson_times_Stochastic_CBC_likelihood(hierarchical_likelihood):
-    def __init__(self, posterior_samples_dict, injections, rate_model, freqs, look_up_Om0, stochastic_data, nparallel=None, neffPE=20, neffINJ=None):
+    def __init__(self, posterior_samples_dict, injections, rate_model, look_up_Om0, stochastic_data, nparallel=None, neffPE=20, neffINJ=None):
+        '''
+        Class for a joint likelihood: hierachical likelihood combined to stochastic likelihood
+        
+        Parameters
+        ----------
+        posterior_samples_dict: dict
+            Dictionary containing the posterior samples class
+        injections: class
+            Injection class from its module 
+        rate_model: class
+            Rate model to compute the CBC rate per year at the detector, taken from the wrapper module.
+            
+        look_up_Om0: dict
+            OmegaGW weights, computed using the stochastic module
+        
+        stochastic_data: dict
+            Dictonary containing stochastic data: Cf (normalised at H0=100), sigmas2 (normalised at H0=100) and freqs. 
+        
+        nparallel: int
+            Number of samples to use per event, if None it will use the maximum number of PE samples in common to all events
+        neffPE: int
+            Effective number of samples per event that must contribute the prior evaluation
+        neffINJ: int
+            Number of effective injections needed to evaluate the selection bias, if None we will assume 4* observed signals.
+        '''
         super().__init__(posterior_samples_dict, injections, rate_model, nparallel, neffPE, neffINJ)
         self.freqs = freqs
         self.look_up_Om0 = look_up_Om0
         self.stochastic_data = stochastic_data
     def log_likelihood(self):
+        '''
+        Evaluates and return the hierarchical log-likelihood
+        '''
         hierarchical_log_likelihood = super().log_likelihood()
         stochastic_log_likelihood = self.stochastic_log_likelihood()
         return hierarchical_log_likelihood + stochastic_log_likelihood
     def stochastic_log_likelihood(self):
-        import time 
+        '''
+        Evaluates and return the stochastic log-likelihood
+        '''
         Cf = self.stochastic_data['Cf']*np.power(self.parameters['H0']/100,-2)
         sigma2s = self.stochastic_data['sigma2s']*np.power(self.parameters['H0']/100,-4)
+        freqs=self.stochastic_data['freqs']
         # Rate model is updated from the call of the poisson likelihood
 
-        omega_gw = spectral_siren_vanilla_omega_gw(self.freqs, self.look_up_Om0, self.rate_model)
+        omega_gw = spectral_siren_vanilla_omega_gw(freqs, self.look_up_Om0, self.rate_model)
 
         # likelihood in eq. 23
-        diff = omega_gw - Cf
-        log_stoch = -0.5 * np.sum(((diff**2.) / sigma2s)) -0.5*np.sum(np.log(2*np.pi*(sigma2s)))
+        diff = np.abs(omega_gw - Cf)
+        log_stoch = -0.5 * np.sum(((diff**2.) / sigma2s)) #-0.5*np.sum(np.log(2*np.pi*(sigma2s)))
         # Controls on the value of the log-likelihood. If the log-likelihood is -inf, then set it to the smallest
         # python value 1e-309
         if log_stoch == np.inf:
@@ -173,5 +206,57 @@ class Poisson_times_Stochastic_CBC_likelihood(hierarchical_likelihood):
             log_stoch = float(np.nan_to_num(log_stoch))
         
         return float(cp2np(log_stoch))
+        
+class Stochastic_likelihood_only(bilby.Likelihood):
+    def __init__(self, rate_model, look_up_Om0, stochastic_data):
+        '''
+        Class for a stochastic likelihood.
+        
+        Parameters
+        ----------
+        rate_model: class
+            Rate model to compute the CBC rate per year at the detector, taken from the wrapper module.
+            
+        look_up_Om0:
+             OmegaGW weights, computed using the stochastic module
+        
+        stochastic_data: dict
+            Dictonary containing stochastic data: Cf (normalised at H0=100), sigmas2 (normalised at H0=100) and freqs. 
+        '''
+        self.rate_model=rate_model
+        self.look_up_Om0 = look_up_Om0
+        self.stochastic_data = stochastic_data
 
-    
+        super().__init__(parameters={ll: None for ll in self.rate_model.population_parameters})
+        
+    def log_likelihood(self):
+        '''
+        Evaluates and return the hierarchical log-likelihood
+        '''
+        #Update the rate model with the population parameters
+        self.rate_model.update(**{key:self.parameters[key] for key in self.rate_model.population_parameters})
+        stochastic_log_likelihood = self.stochastic_log_likelihood()
+        return stochastic_log_likelihood
+    def stochastic_log_likelihood(self):
+        '''
+        Evaluates and return the stochastic log-likelihood
+        '''
+        Cf = self.stochastic_data['Cf']*np.power(self.parameters['H0']/100,-2)
+        sigma2s = self.stochastic_data['sigma2s']*np.power(self.parameters['H0']/100,-4)
+        freqs=self.stochastic_data['freqs']
+        # Rate model is updated from the call of the poisson likelihood
+
+        omega_gw = spectral_siren_vanilla_omega_gw(freqs, self.look_up_Om0, self.rate_model)
+        # likelihood in eq. 23
+        diff = np.abs(omega_gw - Cf)
+        log_stoch = -0.5 * np.sum(((diff**2.) / sigma2s)) #-0.5*np.sum(np.log(2*np.pi*(sigma2s)))
+        # Controls on the value of the log-likelihood. If the log-likelihood is -inf, then set it to the smallest
+        # python value 1e-309
+        if log_stoch == np.inf:
+            raise ValueError('LOG-likelihood must be smaller than infinite')
+        if np.isnan(log_stoch):
+            log_stoch = float(np.nan_to_num(-np.inf))
+        else:
+            log_stoch = float(np.nan_to_num(log_stoch))
+        
+        return float(cp2np(log_stoch))
